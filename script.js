0a57b4372b0a156dac30b00df85c95199a6fa8d0
Anne-Emmanuelle <bouda@iMac-de-Farid.local>
2021-10-18 19:24:37 +0200

    v3

--- script.js
+++ script.js
@@ -1,67 +1,40 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+  $(".container").hover(function () {
+    $(this).siblings("section").css({ filter: "blur(20px)" });
+    $("main").addClass("main_white");
 
-   $(".container").hover(function () {
+    if ($(this).hasClass("gauche")) {
+      $(this).children(".back").addClass("backswapgauche");
+      $(this).children(".back").addClass("backslide");
+    } else if ($(this).hasClass("droite")) {
+      $(this).children(".back").addClass("backswapdroite");
+      $(this).children(".back").addClass("backslide");
+    } else if ($(this).hasClass("droite1")) {
+      $(this).children(".back").addClass("backswapdroite1");
+      $(this).children(".back").addClass("backslide");
+    } else {
+      $(this).children(".back").addClass("backswapcentre");
+      $(this).children(".back").addClass("backslide");
+    }
 
-        $(this).siblings("section").css({"filter": "blur(20px)"});
-        $("main").addClass("main_white");
-
-        if ($(this).hasClass("gauche")){
-            $(this).children(".back").addClass("backswapgauche");
-        }
-        else if ($(this).hasClass("droite")){
-            $(this).children(".back").addClass("backswapdroite");
-        }
-
-        else if ($(this).hasClass("droite1")){
-            $(this).children(".back").addClass("backswapdroite1");
-        }
-
-        else {
-            $(this).children(".back").addClass("backswapcentre");
-            
-        }
-        
-        $(this).children(".front").addClass("frontswap");
-        $(this).addClass("transformation");
-        $(this).css("z-index", "30000");
-        $(this).siblings("section").css("z-index", "11000");
-    }),
-       
+    $(this).children(".front").addClass("frontswap");
+    $(this).addClass("transformation");
+    $(this).css("z-index", "30000");
+    $(this).siblings("section").css("z-index", "11000");
+  }),
     $(".container").mouseleave(function () {
+      $("main").removeClass("main_white");
+      if ($(this).hasClass("gauche")) {
+        $(this).children(".back").removeClass("backswapgauche");
+      } else if ($(this).hasClass("droite")) {
+        $(this).children(".back").removeClass("backswapdroite");
+      } else if ($(this).hasClass("droite1")) {
+        $(this).children(".back").removeClass("backswapdroite1");
+      } else {
+        $(this).children(".back").removeClass("backswapcentre");
+      }
 
-        $("main").removeClass("main_white");
-        if ($(this).hasClass("gauche")){
-           $(this).children(".back").removeClass("backswapgauche");
-        }
-        else if ($(this).hasClass("droite")) {
-            $(this).children(".back").removeClass("backswapdroite"); 
-        }
-        else if ($(this).hasClass("droite1")) {
-            $(this).children(".back").removeClass("backswapdroite1"); 
-        }
-        else {
-            $(this).children(".back").removeClass("backswapcentre"); 
-        } 
-        
-        
-        
-        $(this).children(".front").removeClass("frontswap");
-        $(this).siblings("section").css({"filter": "blur(0px)"});
-          
+      $(this).children(".front").removeClass("frontswap");
+      $(this).siblings("section").css({ filter: "blur(0px)" });
     });
-
 });
-
- 
-    
-
-
-    
-
-
-
-
-
-
-
-
